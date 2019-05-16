36fa8ef1211c88eb76f50fbce3b9755d4a62a3b8
Ahn <potensj@gmail.com>
2019-05-16 18:45:50 +0900

    Create check_empty_list.py

---
+++ python/python-skill/check_empty_list.py
@@ -0,0 +1,14 @@
+'''
+  Check list or Array is empty
+'''
+
+# incorrect solution
+a = []
+if a is None:
+  print('a is empty!')
+ 
+# correct solution
+a = []
+  if not a: 
+    print('a is empty!')
+    
